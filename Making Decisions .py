c444374926ee33ef23cf4c046143c63bcdcf0a09
Albert Egron <47090048+albertegron@users.noreply.github.com>
2019-12-27 16:56:50 -0500

    Update Making Decisions .py

--- Making Decisions .py
+++ Making Decisions .py
@@ -9,7 +9,7 @@ print( "You enter a dark room with two doors. Do you go through door #1 or door 
 door = raw_input("> ")
 
 if door == "1":
-	print ("There's a giant bear here eating a cheese cake. What do you do?")
+	print ("There is a giant bear here eating a cheese cake. What do you do?")
 	print ("1. Take the cake.")
 	print ("2. Scream at the bear.")
 	
